620febfdf65a197d9d8620f78e69c163885f1fb6
Brando Vidal <yisusdracon@gmail.com>
2023-04-29 14:04:09 -0500

    first commit

---
+++ shell/zsh/.zim/init.zsh
@@ -0,0 +1,9 @@
+zimfw() { source /home/brando/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+zmodule() { source /home/brando/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+fpath=(/home/brando/.dotfiles/shell/zsh/.zim/modules/git-info/functions ${fpath})
+autoload -Uz -- coalesce git-action git-info
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
