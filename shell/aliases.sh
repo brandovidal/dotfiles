d8218a1d7e27268c28183f8e5b5db8a7796f338d
Brando Vidal <yisusdracon@gmail.com>
2023-11-04 16:38:54 -0500

    ⚒  export aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,18 +9,53 @@ alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
-alias gaa="git add -A"
+alias gaa='git add .'
+alias gb='git branch'
 alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
+alias gcmsg='git commit -m'
 alias gco="git checkout"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+alias gf='git fetch'
+alias gfa='git fetch --all --prune --jobs=10'
+alias gl='git pull'
+alias glra="git pull --rebase --autostash"
+alias glods='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'' --date=short'
+alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'
+alias glog='$DOTLY_PATH/bin/dot git pretty-log'
+alias gm='git merge'
+alias gp='git push'
+alias gpf="git push --force"
+alias gr='git remote'
+alias grv='git remote -v'
+alias gsw='git switch'
 alias gs="git status -sb"
-alias gf="git fetch --all -p"
-alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull --rebase --autostash"
-alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gss='git status -s'
+
+# NPM Packages
+alias npms="npm start"
+alias npmd="npm run dev"
+alias npmb="npm run build"
+alias npmc="npm run clean"
+alias npml="npm run lint"
+alias npmlf="npm run lint-fix"
+alias npmk="npm run kill"
+
+alias yarns="yarn start"
+alias yarnd="yarn run dev"
+alias yarnb="yarn run build"
+alias yarnl="yarn run lint"
+alias yarnlf="yarn run lint-fix"
+alias yarnc="yarn run clean"
+alias yarnk="yarn run kill"
+
+alias pnpmd="pnpm run dev"
+alias pnpmb="pnpm run build"
+alias pnpmc="pnpm run clean"
+alias pnpms="pnpm start"
+alias pnpml="pnpm run lint"
+alias pnpmlf="pnpm run lint-fix"
+alias pnpmk="pnpm run kill"
 
 # Utils
 alias k='kill -9'
@@ -28,3 +63,8 @@ alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
+
+# Undefined Bootcamp
+alias findd="ls ~/ | grep $1"
+alias cl="clear"
+alias cafe="cat /dev/urandom | hexdump | grep \"ca fe\""
--- shell/zsh/.zshrc
+++ shell/zsh/.zshrc
@@ -32,47 +32,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# alias
-alias gaa='git add .'
-alias gb='git branch'
-alias gcmsg='git commit -m'
-alias gf='git fetch'
-alias gfa='git fetch --all --prune --jobs=10'
-alias gl='git pull'
-alias glods='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'' --date=short'
-alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'
-alias glog='git log --oneline --decorate --graph'
-alias gm='git merge'
-alias gp='git push'
-alias gr='git remote'
-alias grv='git remote -v'
-alias gsw='git switch'
-alias gss='git status -s'
-
-# npm and yarn aliases
-alias npms="npm start"
-alias npmd="npm run dev"
-alias npmb="npm run build"
-alias npmc="npm run clean"
-alias npml="npm run lint"
-alias npmlf="npm run lint-fix"
-alias npmk="npm run kill"
-
-alias yarns="yarn start"
-alias yarnd="yarn run dev"
-alias yarnb="yarn run build"
-alias yarnl="yarn run lint"
-alias yarnlf="yarn run lint-fix"
-alias yarnc="yarn run clean"
-alias yarnk="yarn run kill"
-
-alias pnpmd="pnpm run dev"
-alias pnpmb="pnpm run build"
-alias pnpmc="pnpm run clean"
-alias pnpms="pnpm start"
-alias pnpml="pnpm run lint"
-alias pnpmlf="pnpm run lint-fix"
-alias pnpmk="pnpm run kill"
 # pnpm
 export PNPM_HOME="/home/brando/.local/share/pnpm"
 case ":$PATH:" in
