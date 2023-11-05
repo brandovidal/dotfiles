f83f0f9ef14ee3b1ec99bc9c32b281f882ffc80e
Brando Vidal <yisusdracon@gmail.com>
2023-11-04 19:07:38 -0500

    ⚒  add exports and zsh scripts

--- shell/exports.sh
+++ shell/exports.sh
@@ -31,19 +31,6 @@ fi
 
 export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
 
-# pnpm
-export PNPM_HOME="/home/brando/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
-
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # ------------------------------------------------------------------------------
 # Path - The higher it is, the more priority it has
 # ------------------------------------------------------------------------------
