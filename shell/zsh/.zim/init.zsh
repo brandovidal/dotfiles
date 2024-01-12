ae358b0f370b55964c8927d5796a04859c032147
Brando Vidal <yisusdracon@gmail.com>
2024-01-12 07:07:44 -0500

    🔨  fix using zmodule

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,14 +1,13 @@
 zimfw() { source /home/brando/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /home/brando/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-fpath=(/home/brando/.dotfiles/shell/zsh/.zim/modules/git-info/functions ${fpath})
-autoload -Uz -- coalesce git-action git-info
+fpath=(/home/brando/.dotfiles/shell/zsh/.zim/modules/git-info/functions /home/brando/.dotfiles/shell/zsh/.zim/modules/prompt-pwd/functions ${fpath})
+autoload -Uz -- coalesce git-action git-info prompt-pwd
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-sudo/init.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/exa/init.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-completions/zsh-completions.plugin.zsh
-source /home/brando/.dotfiles/shell/zsh/.zim/modules/history/init.zsh
 source /home/brando/.dotfiles/shell/zsh/.zim/modules/git-prune/git-prune.plugin.zsh
+source /home/brando/.dotfiles/shell/zsh/.zim/modules/zsh-sudo/init.zsh
